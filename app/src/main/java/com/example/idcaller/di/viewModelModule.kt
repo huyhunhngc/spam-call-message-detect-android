f06830d2ad217383886ccfa0892e2dad369413e4
huyhuynhHN <huyhn@hybrid-technologies.vn>
2022-03-23 00:36:34 +0700

    feat: base bottom bar

--- app/src/main/java/com/example/idcaller/di/viewModelModule.kt
+++ app/src/main/java/com/example/idcaller/di/viewModelModule.kt
@@ -1,7 +1,9 @@
 package com.example.idcaller.di
 
+import com.example.idcaller.presentation.main.callTab.CallTabViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
-
+    viewModel { CallTabViewModel() }
 }
---
+++ app/src/main/java/com/example/idcaller/presentation/main/callTab/CallTabViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.idcaller.presentation.main.callTab
+
+import com.example.idcaller.core.base.BaseViewModel
+
+class CallTabViewModel: BaseViewModel() {
+    override fun onStart() {
+        super.onStart()
+    }
+}
