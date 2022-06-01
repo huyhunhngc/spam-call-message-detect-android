703dd9636e25f780d8a7f0bd85274422d0ee5f57
huyhunhngc <huyhn@hybrid-technologies.vn>
2022-06-01 16:37:57 +0700

    capture noti

---
+++ app/src/main/java/com/dotsdev/idcaller/data/broadcast/CaptureNotificationListener.kt
@@ -0,0 +1,25 @@
+package com.dotsdev.idcaller.data.broadcast
+
+import android.service.notification.NotificationListenerService
+import android.service.notification.StatusBarNotification
+import android.text.TextUtils
+import android.util.Log
+
+
+class CaptureNotificationListener : NotificationListenerService() {
+    override fun onNotificationPosted(sbn: StatusBarNotification) {
+        val packageName = sbn.packageName
+        if (!TextUtils.isEmpty(packageName)) {
+            Log.d("!@#", "onNotificationPosted: ${sbn.notification}")
+        }
+    }
+
+
+    override fun onNotificationRemoved(sbn: StatusBarNotification) {
+        val packageName = sbn.packageName
+        if (!TextUtils.isEmpty(packageName)) {
+            Log.d("!@#", "onNotificationRemoved ${sbn.notification.tickerText}" +
+                    "${sbn.notification.category}")
+        }
+    }
+}
