313fafdee651ad681221c975b4170d346bb5dcaa
laptoptt <hnmnhnmn2@gmail.com>
2022-06-25 17:22:23 +0700

    important spam message

--- app/src/main/java/com/dotsdev/idcaller/presentation/main/messagetab/tablayout/ImportantMessageFragment.kt
+++ app/src/main/java/com/dotsdev/idcaller/presentation/main/messagetab/tablayout/ImportantMessageFragment.kt
@@ -1,9 +1,13 @@
 package com.dotsdev.idcaller.presentation.main.messagetab.tablayout
 
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.dotsdev.idcaller.R
 import com.dotsdev.idcaller.core.base.BaseFragment
 import com.dotsdev.idcaller.core.base.viewBindings
 import com.dotsdev.idcaller.databinding.FragmentImportantMessageBinding
+import com.dotsdev.idcaller.utils.toDetail
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class ImportantMessageFragment :
@@ -13,6 +17,19 @@ class ImportantMessageFragment :
     override val binding: FragmentImportantMessageBinding by viewBindings(
         FragmentImportantMessageBinding::bind
     )
+    override fun onViewCreated(view: View, savedInstanceState: Bundle? ){
+        super.onViewCreated(view, savedInstanceState)
+        binding.viewModel = viewModel
+        observers()
+    }
+
+    private fun observers() {
+        with(viewModel){
+            detailClick.observe(viewLifecycleOwner){
+                it.toDetail()?.let(findNavController()::navigate)
+            }
+        }
+    }
 
     companion object {
         fun newInstance(): ImportantMessageFragment {
--- app/src/main/java/com/dotsdev/idcaller/presentation/main/messagetab/tablayout/SpamMessageFragment.kt
+++ app/src/main/java/com/dotsdev/idcaller/presentation/main/messagetab/tablayout/SpamMessageFragment.kt
@@ -1,9 +1,13 @@
 package com.dotsdev.idcaller.presentation.main.messagetab.tablayout
 
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.dotsdev.idcaller.R
 import com.dotsdev.idcaller.core.base.BaseFragment
 import com.dotsdev.idcaller.core.base.viewBindings
 import com.dotsdev.idcaller.databinding.FragmentSpamMessageBinding
+import com.dotsdev.idcaller.utils.toDetail
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SpamMessageFragment : BaseFragment<SpamMessageViewModel, FragmentSpamMessageBinding>
@@ -12,6 +16,20 @@ class SpamMessageFragment : BaseFragment<SpamMessageViewModel, FragmentSpamMessa
     override val binding: FragmentSpamMessageBinding by viewBindings(
         FragmentSpamMessageBinding::bind
     )
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?){
+        super.onViewCreated(view, savedInstanceState)
+        binding.viewModel = viewModel
+        observers()
+    }
+
+    private fun observers() {
+        with(viewModel) {
+            detailClick.observe(viewLifecycleOwner){
+                it.toDetail()?.let(findNavController()::navigate)
+            }
+        }
+
+    }
 
     companion object {
         fun newInstance(): SpamMessageFragment {
